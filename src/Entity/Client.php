4b3e82204388395a74ad34e4704d57c33784fd83
naesis <1naesis@gmail.com>
2021-10-13 19:19:41 +0300

    Client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,295 +2,137 @@
 
 namespace App\Entity;
 
-use App\Repository\ClientRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=ClientRepository::class)
+ * Client
+ *
+ * @ORM\Table(name="client")
+ * @ORM\Entity
  */
 class Client
 {
     /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="client_id_seq", allocationSize=1, initialValue=1)
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=20, nullable=false)
      */
     private $phone;
 
     /**
-     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="updated", type="datetime", nullable=true)
      */
     private $updated;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="state", type="string", length=20, nullable=true)
      */
     private $state;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="operator", type="string", length=20, nullable=true)
      */
     private $operator;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="region", type="string", length=64, nullable=true)
      */
     private $region;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="names", type="string", length=255, nullable=true)
      */
     private $names;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="vk", type="string", length=128, nullable=true)
      */
     private $vk;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="insta", type="string", length=128, nullable=true)
      */
     private $insta;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="fb", type="string", length=128, nullable=true)
      */
     private $fb;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="email", type="string", length=64, nullable=true)
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="day_birdth", type="string", length=16, nullable=true)
      */
     private $dayBirdth;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="tg", type="string", length=128, nullable=true)
      */
     private $tg;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="whatsapp", type="string", length=128, nullable=true)
      */
     private $whatsapp;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="viber", type="string", length=128, nullable=true)
      */
     private $viber;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="ok", type="string", length=128, nullable=true)
      */
     private $ok;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
+     *
+     * @ORM\Column(name="adress", type="string", length=700, nullable=true)
      */
     private $adress;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
-    public function getPhone(): ?string
-    {
-        return $this->phone;
-    }
-
-    public function setPhone(string $phone): self
-    {
-        $this->phone = $phone;
-
-        return $this;
-    }
-
-    public function getUpdated(): ?\DateTimeInterface
-    {
-        return $this->updated;
-    }
-
-    public function setUpdated(?\DateTimeInterface $updated): self
-    {
-        $this->updated = $updated;
-
-        return $this;
-    }
-
-    public function getState(): ?string
-    {
-        return $this->state;
-    }
-
-    public function setState(?string $state): self
-    {
-        $this->state = $state;
-
-        return $this;
-    }
-
-    public function getOperator(): ?string
-    {
-        return $this->operator;
-    }
-
-    public function setOperator(?string $operator): self
-    {
-        $this->operator = $operator;
-
-        return $this;
-    }
-
-    public function getRegion(): ?string
-    {
-        return $this->region;
-    }
-
-    public function setRegion(?string $region): self
-    {
-        $this->region = $region;
-
-        return $this;
-    }
-
-    public function getNames(): ?string
-    {
-        return $this->names;
-    }
-
-    public function setNames(?string $names): self
-    {
-        $this->names = $names;
-
-        return $this;
-    }
-
-    public function getVk(): ?string
-    {
-        return $this->vk;
-    }
-
-    public function setVk(string $vk): self
-    {
-        $this->vk = $vk;
-
-        return $this;
-    }
-
-    public function getInsta(): ?string
-    {
-        return $this->insta;
-    }
-
-    public function setInsta(?string $insta): self
-    {
-        $this->insta = $insta;
-
-        return $this;
-    }
-
-    public function getFb(): ?string
-    {
-        return $this->fb;
-    }
-
-    public function setFb(?string $fb): self
-    {
-        $this->fb = $fb;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(?string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getDayBirdth(): ?string
-    {
-        return $this->dayBirdth;
-    }
-
-    public function setDayBirdth(?string $dayBirdth): self
-    {
-        $this->dayBirdth = $dayBirdth;
-
-        return $this;
-    }
-
-    public function getTg(): ?string
-    {
-        return $this->tg;
-    }
-
-    public function setTg(?string $tg): self
-    {
-        $this->tg = $tg;
-
-        return $this;
-    }
-
-    public function getWhatsapp(): ?string
-    {
-        return $this->whatsapp;
-    }
-
-    public function setWhatsapp(?string $whatsapp): self
-    {
-        $this->whatsapp = $whatsapp;
-
-        return $this;
-    }
-
-    public function getViber(): ?string
-    {
-        return $this->viber;
-    }
-
-    public function setViber(?string $viber): self
-    {
-        $this->viber = $viber;
-
-        return $this;
-    }
-
-    public function getOk(): ?string
-    {
-        return $this->ok;
-    }
-
-    public function setOk(?string $ok): self
-    {
-        $this->ok = $ok;
-
-        return $this;
-    }
-
-    public function getAdress(): ?string
-    {
-        return $this->adress;
-    }
-
-    public function setAdress(?string $adress): self
-    {
-        $this->adress = $adress;
-
-        return $this;
-    }
 }
