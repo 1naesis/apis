d26f2bc99ad64aa0b42213ad21c0b1fbde717be5
naesis <1naesis@gmail.com>
2021-10-30 21:21:57 +0300

    [Checkphone] add table Images

---
+++ src/Entity/Checkphone/Images.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity\Checkphone;
+
+use App\Repository\Checkphone\ImagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ImagesRepository::class)
+ */
+class Images
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=700)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $client_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getClientId(): ?int
+    {
+        return $this->client_id;
+    }
+
+    public function setClientId(int $client_id): self
+    {
+        $this->client_id = $client_id;
+
+        return $this;
+    }
+}
